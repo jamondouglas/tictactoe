1eb928bb1d24d97221434b01c2a43cbc1feeb887
jamondouglas <terrildouglas@gmail.com>
2015-01-07 01:01:43 -0800

    Code clean up of angular app module

--- client/app.js
+++ client/app.js
@@ -1,19 +1,19 @@
 angular.module('starter',[
-'ui.bootstrap',
-'ngRoute',
-'tictactoe',
-'TicTacToe.Game',
-'TicTacToe.Board',
-'TicTacToe.Player'
+  'ui.bootstrap',
+  'ngRoute',
+  'tictactoe',
+  'TicTacToe.Game',
+  'TicTacToe.Board',
+  'TicTacToe.Player'
 ])
-.constant('gridSize',3)
-.config(function($routeProvider, $httpProvider){
-$routeProvider
-.when('/',{
-templateUrl:'views/tictactoe.html',
-controller:'TicTacToeController'
-})
-.otherwise({
-redirectTo: '/'
-});
-});
+  .constant('gridSize',3)
+  .config(function($routeProvider){
+    $routeProvider
+    .when('/',{
+      templateUrl:'views/tictactoe.html',
+      controller:'TicTacToeController'
+    })
+  .otherwise({
+      redirectTo: '/'
+    });
+  });
